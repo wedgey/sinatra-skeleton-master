091f51df0fa3af1123cb7482b59d0827a82fa956
wedgey <desmondpoon5@hotmail.com>
2016-07-08 23:31:08 +0000

    Added support for urls

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,14 @@
 class Message < ActiveRecord::Base
-  # validates :title, presence: true
   validates :author, presence: true
   validates :content, presence: true
+
+  validate :valid_url
+
+private
+
+  def valid_url
+    unless url =~ /\bhttps?:\/\/?www\..+\.\w+\b/
+      errors.add(:url, "should have a valid url")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708214600) do
+ActiveRecord::Schema.define(version: 20160708225533) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
     t.string "author"
     t.string "content"
+    t.string "url"
   end
 
 end
